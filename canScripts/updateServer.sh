d38e365a89e999f6c28445153d9aa1a12829f25e
Can Yildirim <canpy30@gmail.com>
2015-10-12 18:30:22 -0700

    updated bootUpScript

--- canScripts/updateServer.sh
+++ canScripts/updateServer.sh
@@ -1,7 +1,9 @@
  #!/bin/bash
 
  cd /opt/mean;
+ sudo pm2 stop grunt;
  sudo git checkout server-deployment;
  sudo git fetch origin;
  sudo git pull;
+ sudo pm2 start grunt;
 echo "GITHUB SERVER DEPLOYMENT COMPLETE";
