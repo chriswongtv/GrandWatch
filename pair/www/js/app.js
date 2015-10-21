94d5c04d29959a2e27a6479bc954b77654aa8d8f
Chris Wong <chris@cogifire.com>
2015-10-21 13:47:51 -0700

    routes

--- pair/www/js/app.js
+++ pair/www/js/app.js
@@ -1,11 +1,4 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('pair', ['ionic', 'pair.controllers', 'pair.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -50,31 +43,22 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     }
   })
 
-  .state('tab.chats', {
-      url: '/chats',
+  .state('tab.sharing', {
+      url: '/sharing',
       views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
+        'tab-sharing': {
+          templateUrl: 'templates/tab-sharing.html',
+          controller: 'SharingCtrl'
         }
       }
     })
 
-  .state('tab.account', {
-    url: '/account',
+  .state('tab.me', {
+    url: '/me',
     views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
+      'tab-me': {
+        templateUrl: 'templates/tab-me.html',
+        controller: 'MeCtrl'
       }
     }
   });
--- pair/www/js/controllers.js
+++ pair/www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', [])
+angular.module('pair.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
 
